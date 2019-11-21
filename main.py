cc6f7598638b5bd7dcd02a2f34e8c9d9b398717f
Matt Clendenen <clendenen.matthew@gmail.com>
2019-11-20 23:48:54 -0600

    CR-04 if target is set, then match the source address, else all source addresses

--- main.py
+++ main.py
@@ -7,33 +7,20 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--target', help='targeted mac address to track')
+#TODO make this required
 parser.add_argument('-i', '--iface', help='wireless interface to listen with')
 args = parser.parse_args()
 
+#TODO do some exception catching
 
 def pkt_callback(pkt):
-    # print(pkt[0].show())
-    # radiotap = pkt.getlayer(RadioTap)
-    # rssi = radiotap.dBm_AntSignal
-    # print(rssi)
-    # print("\n")
-    # record = object()
-    # print(record)
-    # print(args.iface)
-    if pkt.type == 0 and pkt.subtype == 4 and pkt.addr2 == args.target:
-        # pkt_record = object()
-        # pkt_record.src = pkt.addr2
-        # pkt_record.rssi = pkt.getlayer(RadioTap).dBm_AntSignal
-        # print(pkt_record)
+    # management frames
+    # probe requests
+    # if target is set, match the source address
+    # else all source addresses
+    if pkt.type == 0 and pkt.subtype == 4 and ((args.target is not None and pkt.addr2 == args.target) or args.target is None):
         probe_request = ProbeRequest(pkt.addr2, pkt.addr1, pkt.addr3, pkt.getlayer(RadioTap).dBm_AntSignal, time.time())
-        print(probe_request)
         print(json.dumps(probe_request.__dict__))
-        # print("\n")
-        # radiotap = pkt.getlayer(RadioTap)
-        # rssi = radiotap.dBm_AntSignal
-        # print(rssi)
-        # print("\n")
-
 
 def main():
     sniff(iface=args.iface, prn=pkt_callback)
