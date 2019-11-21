2e8a4c29646cba4755b9280c4c850601aa595d5c
Matt Clendenen <clendenen.matthew@gmail.com>
2019-11-20 22:13:14 -0600

    basic argument parsing with short options

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
-from scapy.all import sniff
+from scapy.all import sniff, RadioTap
 import getopt, sys
 import argparse
+# from probe_request import ProbeRequest
 
-# small comment to satisfy git?
-
-# the harder manual way
-
-# argsList = sys.argv[1:]
-# unixOptions = "hi:v"
-# gnuOptions = ["help", "isolate=", "verbose"]
-
-# try:
-#     arguments, values = getopt.getopt(argsList, unixOptions, gnuOptions)
-# except getopt.error as err:
-#     # output error, and return with an error code
-#     print (str(err))
-#     sys.exit(2)
-
-# print(arguments)
-# print(arguments[0])
-# print(values)
-# print(values[0])
-
-# probably the better way
 parser = argparse.ArgumentParser()
-parser.add_argument('--isolate', help='isolated mac address to track')
+parser.add_argument('-t', '--target', help='targeted mac address to track')
+parser.add_argument('-i', '--iface', help='wireless interface to listen with')
 args = parser.parse_args()
 
 
 def pkt_callback(pkt):
-    # print(type(pkt))
-    if pkt.type == 0 and pkt.subtype == 4 and pkt.addr2 == args.isolate:
+    # # print(type(pkt))
+    # radiotap = pkt.getlayer(RadioTap)
+    # rssi = radiotap.dBm_AntSignal
+    # print(rssi)
+    # print("\n")
+    # record = object()
+    # print(record)
+    # print(args.iface)
+    if pkt.type == 0 and pkt.subtype == 4 and pkt.addr2 == args.target:
+        # pkt_record = object()
+        # pkt_record.src = pkt.addr2
+        # pkt_record.rssi = pkt.getlayer(RadioTap).dBm_AntSignal
+        # print(pkt_record)
         print(pkt.addr2)
         print("\n")
+        radiotap = pkt.getlayer(RadioTap)
+        rssi = radiotap.dBm_AntSignal
+        print(rssi)
+        # print("\n")
 
 
 def main():
-    sniff(iface='wlan1mon', prn=pkt_callback)
+    sniff(iface=args.iface, prn=pkt_callback)
     
 if __name__ == '__main__':
     main()
